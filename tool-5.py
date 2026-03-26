86432d3846ef348a4eb6cc6d55937b6c078a814c
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-03-26 10:05:35 -0300

    up file, tool no finish

---
+++ tool-5.py
@@ -0,0 +1,21 @@
+from faker import Faker
+
+queue = []
+
+cooldown_client = 0
+cooldown_atention = 0
+
+from faker import Faker
+
+fake = Faker()
+
+#queue.append({
+#    "name": fake.name(),
+#    "age": fake.random_int(min=18, max=80)
+#})
+
+def simulate_client(cooldown_client, cooldown_atention):
+    pass
+
+
+
