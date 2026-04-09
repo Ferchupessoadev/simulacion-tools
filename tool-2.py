acfca0821bd085a7a77d262ef6c6dc5fadc456f4
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2026-04-09 08:29:31 -0300

    Mejorar la simulación de interés compuesto con formato de impresión y simplificación de la lógica del bucle

--- tool-2.py
+++ tool-2.py
@@ -2,7 +2,7 @@ a = int(input("Ingrese cuanto dinero depositará: "))
 b = int(input("Ingrese el porcentaje de interés: "))
 c = int(input("Ingrese la cantidad de ciclos: "))
 d = 1
-while (d<c):
-    a = a*b/100+a
-    d=d+1
-    print("en este ciclo, el dinero es "+str(a))
+while (d < c):
+    a = a * b / 100 + a
+    print(f"en el ciclo {d}, el dinero es {a}")
+    d += 1
